a7b5f3b81bc8d723e6977cba1b62741fb67c6b35
Arsenii Telesh <telesh.sen98@gmail.com>
2023-05-24 01:48:16 +0600

    made homework-3

--- src/channel.py
+++ src/channel.py
@@ -12,33 +12,36 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self._channel_id = channel_id
 
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other: 'Channel') -> int:
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other: 'Channel') -> int:
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __mul__(self, other: 'Channel') -> int:
+        return int(self.subscriber_count) * int(other.subscriber_count)
+
+    def __truediv__(self, other: 'Channel') -> float:
+        return int(self.subscriber_count) / int(other.subscriber_count)
+
+    def __lt__(self, other: 'Channel') -> bool:
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other: 'Channel') -> bool:
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __gt__(self, other) -> bool:
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other: 'Channel') -> bool:
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
     @property
     def channel_id(self):
         return self._channel_id
-
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-
-        api_key: str = os.getenv('YT_API_KEY')
-        # создать специальный объект для работы с API
-        youtube = build('youtube', 'v3', developerKey=api_key)
-
-        def printj(dict_to_print: dict) -> None:
-            """Выводит словарь в json-подобном удобном формате с отступами"""
-            print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-
-        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
-        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        printj(channel)
-
-    @staticmethod
-    def get_service():
-        """Возвращает объект для работы с API"""
-        return build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
-
-    def get_data(self, channel_id='UC-OVMPlMA3-YCIeg4z5z23A'):
-        channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
-        return channel
 
     @property
     def title(self):
@@ -75,6 +78,30 @@ class Channel:
         """Возвращает общее количество просмотров видео на канале"""
         view_count = self.get_data()['items'][0]['statistics']['viewCount']
         return view_count
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+
+        api_key: str = os.getenv('YT_API_KEY')
+        # создать специальный объект для работы с API
+        youtube = build('youtube', 'v3', developerKey=api_key)
+
+        def printj(dict_to_print: dict) -> None:
+            """Выводит словарь в json-подобном удобном формате с отступами"""
+            print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
+        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        printj(channel)
+
+    @staticmethod
+    def get_service():
+        """Возвращает объект для работы с API"""
+        return build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
+
+    def get_data(self):
+        channel = self.get_service().channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel
 
     def to_json(self, name_of_file):
         data = {
